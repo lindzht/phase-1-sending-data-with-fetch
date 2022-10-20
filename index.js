95dcf8ead16b2498d5128e8d359b2f10144b7af0
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-10-20 16:46:33 -0400

    build out POST fetch() and submit event for form

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+
+
+const form = document.querySelector("form")
+// console.log(form);
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    const newDogObject = {
+        dogName: e.target.dogName.value,
+        dogBreed: e.target.dogBreed.value
+    }
+    
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(newDogObject),
+    }
+    .then (response => response.json()),
+    .then 
+    );
+
+    form.reset();
+}
+);
+
+
+
+
+
+
+
+// const submitData = {
+//     name: "TestName",
+//     email: "test@test.com",
+// }
